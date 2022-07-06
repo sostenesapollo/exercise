2aae3ec069c5da196e2272a4e0f62bd5933a7e69
Sóstenes Apollo <sostenesapollo25@gmail.com>
2022-07-06 10:59:11 -0300

    fix: jest path mapping

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@repositories/(.*)$': '<rootDir>/src/repositories/$1',
+    '^@constants/(.*)$': '<rootDir>/src/constants/$1',
+    '^@entities/(.*)$': '<rootDir>/src/entities/$1',
+    '^@repositories/(.*)$': '<rootDir>/src/repositories/$1',
+    '^@util/(.*)$': '<rootDir>/src/util/$1',
+  },
 };
